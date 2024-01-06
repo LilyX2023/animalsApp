aa772d7e0c49fb1c5db359afe7fb03273f162ebe
RachelY <yang.yijing@outlook.com>
2024-01-06 00:27:16 -0500

    server is listening

--- server.js
+++ server.js
@@ -18,5 +18,48 @@ mongoose.connection
 .on("close", () => {console.log("Disconnected from Mongoose")})
 .on("error", (error) => {console.log(error)})
 
+///////////////////////////////////
+//Schema
+const {Schema, model} = mongoose
+const animalSchema = new Schema({
+    species: String,
+    extinct: Boolean,
+    location: String,
+    lifeExpectancy: Number
+})
+
+//Model
+const Animal = model("Animal", animalSchema)
+
+/////////////////////////////////////
+//Express App object
+const app = express()
+
+//////////////////////////////////////
+//Middleware 
+app.use(morgan("dev")); 
+app.use(methodOverride("_method"))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static("public"))
+
+///////////////////////////////////////
+//Routes
+app.get('/',(req, res) => {
+    res.send('Server is running')
+})
+
+
+///////////////////////////////////////////////////////
+// Server Listener
+////////////////////////////////////////////////////////
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`)
+})
+
+
+
+
+
 
 
